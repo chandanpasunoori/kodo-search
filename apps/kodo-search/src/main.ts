c6c74a871c88d0cc585b5cfb14839008d6f94eab
Chandan Pasunoori <chandanpasunoori@gmail.com>
2021-06-04 03:48:28 +0530

    cors and swagger ui added

--- apps/kodo-search/src/main.ts
+++ apps/kodo-search/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Kodo Search')
+    .setDescription('Kodo Search')
+    .setVersion('0.1')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig, {
+    ignoreGlobalPrefix: false,
+  });
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
